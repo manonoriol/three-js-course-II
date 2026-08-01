e18b064a1ae4c6ff61ecb3e2766497b6c169fe1f
Manon Oriol <oriol.manon@gmail.com>
2026-08-01 12:55:28 +0200

    lesson-4-progress

--- 04-transform-objects/src/script.js
+++ 04-transform-objects/src/script.js
@@ -14,6 +14,28 @@ const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+//Position
+// mesh.position.z = - 1
+// mesh.position.y = 0.8
+// mesh.position.x = - 0.6
+mesh.position.set(- 0.6, 0.8, - 0.6)
+
+//Scale
+// mesh.position.x = 2
+// mesh.position.y = 0.5
+// mesh.position.z = - 0.5
+mesh.scale.set(2, 0.5, 0.5)
+
+//Rotation
+mesh.rotation.reorder('YXZ')
+mesh.rotation.x = Math.PI * 0.25
+mesh.rotation.y = Math.PI * 0.25
+
+
+//Axes helper
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
+
 /**
  * Sizes
  */
@@ -27,6 +49,8 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.position.y = 1
+camera.position.x = 1
 scene.add(camera)
 
 /**
