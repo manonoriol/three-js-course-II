0f829342ef600b23baad94060f703903ed0139a6
Manon Oriol <oriol.manon@gmail.com>
2026-08-03 12:34:09 +0200

    complete lesson 04

--- 04-transform-objects/src/script.js
+++ 04-transform-objects/src/script.js
@@ -9,31 +9,34 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group()
+group.scale.y = 2
+group.rotation.y = 0.2
+scene.add(group)
 
-//Position
-// mesh.position.z = - 1
-// mesh.position.y = 0.8
-// mesh.position.x = - 0.6
-mesh.position.set(- 0.6, 0.8, - 0.6)
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0xff0000 })
+)
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0xff00 })
+)
 
-//Scale
-// mesh.position.x = 2
-// mesh.position.y = 0.5
-// mesh.position.z = - 0.5
-mesh.scale.set(2, 0.5, 0.5)
+cube2.position.x = -2
 
-//Rotation
-mesh.rotation.reorder('YXZ')
-mesh.rotation.x = Math.PI * 0.25
-mesh.rotation.y = Math.PI * 0.25
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x0000ff })
+)
 
+cube3.position.x = 2
+
+
+group.add(cube1, cube2, cube3)
 
 //Axes helper
-const axesHelper = new THREE.AxesHelper()
+const axesHelper = new THREE.AxesHelper(2)
 scene.add(axesHelper)
 
 /**
@@ -49,9 +52,8 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-camera.position.y = 1
-camera.position.x = 1
 scene.add(camera)
+
 
 /**
  * Renderer
