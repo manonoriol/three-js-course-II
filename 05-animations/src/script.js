c428894f89a93d7afaaf420152919bac635c250c
Manon Oriol <oriol.manon@gmail.com>
2026-08-07 12:54:03 +0200

    clock done

--- 05-animations/src/script.js
+++ 05-animations/src/script.js
@@ -29,19 +29,16 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 
-//Time
-let time = Date.now()
+//Clock
+const clock = new THREE.Clock()
 
 //Animations
 const tick = () => {
-
-    //Time
-    const currentTime = Date.now()
-    const deltaTime = currentTime - time
-    time = currentTime
-
+    //Clock
+    const elapsedTime = clock.getElapsedTime()
     //Update objects
-    mesh.rotation.y += 0.001 * deltaTime
+    mesh.position.y = Math.sin(elapsedTime)
+    mesh.position.x = Math.cos(elapsedTime)
 
     //Render
     renderer.render(scene, camera)
