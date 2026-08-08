41fde5bf733edb580b49a83027f2dc59f3a39f24
Manon Oriol <oriol.manon@gmail.com>
2026-08-08 16:09:34 +0200

    completed lesson 05

--- 05-animations/src/script.js
+++ 05-animations/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import gsap from 'gsap'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -29,20 +30,30 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 
+
+
+//Gsap
+
+gsap.to(mesh.position, {duration: 1, delay: 1, x: 2})
+gsap.to(mesh.position, {duration: 1, delay: 2, x: 0})
+
 //Clock
-const clock = new THREE.Clock()
+// const clock = new THREE.Clock()
 
 //Animations
 const tick = () => {
     //Clock
-    const elapsedTime = clock.getElapsedTime()
+    // const elapsedTime = clock.getElapsedTime()
     //Update objects
-    mesh.position.y = Math.sin(elapsedTime)
-    mesh.position.x = Math.cos(elapsedTime)
+    // mesh.position.y = Math.sin(elapsedTime)
+    // mesh.position.x = Math.cos(elapsedTime)
+
 
     //Render
     renderer.render(scene, camera)
+
     window.requestAnimationFrame(tick)
 }
 
 tick()
+
